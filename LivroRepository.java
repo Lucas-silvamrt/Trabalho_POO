49ea5e79d261bdf1528d2ac7c89efdba05b93c4e
Lucas Silva <lucassilva31102006@gmail.com>
2025-12-09 13:59:52 -0300

    Delete LivroRepository.java

--- LivroRepository.java
+++
@@ -1,5 +0,0 @@
-package com.biblioteca.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LivroRepository extends JpaRepository<biblioteca.Livro, Long> {}
