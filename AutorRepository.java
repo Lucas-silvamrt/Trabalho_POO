15e8e8b16a4582c210432c99bb5809b6243a39fa
Lucas Silva <lucassilva31102006@gmail.com>
2025-12-09 13:58:42 -0300

    Delete AutorRepository.java

--- AutorRepository.java
+++
@@ -1,5 +0,0 @@
-package com.biblioteca.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AutorRepository extends JpaRepository<biblioteca.Autor, Long> {}
