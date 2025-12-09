acf3e7672a03f5cf88ade97712e3d2dfac1fc0fb
Lucas Silva <lucassilva31102006@gmail.com>
2025-12-09 13:59:27 -0300

    Delete EditoraRepository.java

--- EditoraRepository.java
+++
@@ -1,5 +0,0 @@
-package com.biblioteca.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EditoraRepository extends JpaRepository<biblioteca.Editora, Long> {}
